58c3db55ef2714abbbace479e1da9242e07fa2b3
valaczkailona <valaczka.ilona@gmail.com>
2024-05-02 12:46:44 +0200

    Add a button to make a new sized grid by user

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 
 
 First I made a 16x16 grid by the function drawGrid
+
 I set hover effect that the grid divs change color when my mouse passes over them
+
+Finally I added a button that sends the user a prompt, 
+and the answer is the new grid size
--- script.js
+++ script.js
@@ -1,26 +1,47 @@
+
 const grid = document.querySelector('#container');
-const gridSize = 16;
+const btn = document.querySelector('button');
+
+let gridSize = 16
+drawGrid(gridSize);
+
+btn.addEventListener('click', () => {
+    gridSize = +(prompt("Add a mumber between 1-100", ''));
+
+    if (gridSize > 0 && gridSize<= 100) {
+        drawGrid(gridSize);
+    } else if (gridSize <= 0 ||
+                gridSize > 100 ||
+                isNaN(gridSize))  {
+        gridSize = +(prompt("Add a mumber between 1-100", ''));
+    }    
+});
+
 
 function drawGrid(gridSize) {
-
+ 
+    grid.innerHTML = "";
+    
     for (let i = 1; i <= gridSize; i++) {
         const row = document.createElement('div');
         row.classList.add("row");
-        grid.appendChild(row);
+       
         //draw column divs whitin each raw
         for (let j = 1; j <= gridSize; j++){
-            const column = document.createElement('div');
-            column.classList.add("column");
-            row.appendChild(column);
+            const square = document.createElement('div');
+            square.classList.add("column");
+            row.appendChild(square);
         }
+        grid.appendChild(row);
     }
+    
     return grid;
 };
-drawGrid(gridSize);
 
 grid.addEventListener('mouseover', (e) => {
     const gridPart = e.target;
     gridPart.style.backgroundColor = "black";
 })
+
 
 
